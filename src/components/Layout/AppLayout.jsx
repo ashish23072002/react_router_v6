9c565e01930d1d9a6fd6d81c4fc7ae4fa8dbd6bb
ashish23072002 <ashish23bisht@gmail.com>
2025-07-14 19:42:43 +0530

    Create Global Loading States

--- src/components/Layout/AppLayout.jsx
+++ src/components/Layout/AppLayout.jsx
@@ -1,9 +1,12 @@
 
 import Header from './Header'
 import Footer from './Footer'
-import { Outlet } from 'react-router'
+import { Outlet, useNavigate } from 'react-router'
+import { Loading } from './Loading';
 
 function AppLayout() {
+  const navigate=useNavigate();
+  if(navigate.state==="loading") return <Loading/>
   return (
     <div>
       <Header/>
