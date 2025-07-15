839cb0d0240d0618ae56a28d1188d8f47e579dd6
ashish23072002 <ashish23bisht@gmail.com>
2025-07-15 19:21:36 +0530

    Secure Your React App: Using ENV Variables

--- src/api/GetApiData.jsx
+++ src/api/GetApiData.jsx
@@ -1,9 +1,11 @@
 export const getMoviesData = async () => {
   try {
     const response = await fetch(
-      "https://www.omdbapi.com/?i=tt3896198&apikey=47aee04e&s=titanic&page=1"
+      `https://www.omdbapi.com/?i=tt3896198&apikey=${
+        import.meta.env.VITE_API_KEY
+      }&s=titanic&page=1`
     );
-    const data=response.json();
+    const data = response.json();
     return data;
   } catch (error) {
     console.log(error);
